f972063d5ffe62fc4f3a067281f2955e411cd8c9
Gaurang Patel <gaurang.patel@qualtab.com>
2022-11-16 15:14:22 +0530

    Added ignore for json properties

--- sharepoint_api/src/main/java/com/microsoft/dto/sharepoint/UserDetails.java
+++ sharepoint_api/src/main/java/com/microsoft/dto/sharepoint/UserDetails.java
@@ -1,8 +1,11 @@
 package com.microsoft.dto.sharepoint;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 import lombok.Data;
 
 @Data
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class UserDetails {
 
 	private User user;
